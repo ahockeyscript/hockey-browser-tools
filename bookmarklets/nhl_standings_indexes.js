bd5517d37612edfda16759bc69c6b2dc65cfcef2
ahockeyscript <ahockeyscript@gmail.com>
2018-03-17 16:16:37 +0800

    Adding the Standings Indexes bookmarklet

---
+++ bookmarklets/nhl_standings_indexes.js
@@ -0,0 +1,1 @@
+javascript:(function($) { $('head').append('<style type="text/css">.standings--datatable table { counter-reset: row-num; } .standings--datatable table tbody tr { counter-increment: row-num; } .standings--datatable table tbody tr td:first-child::before { content: counter(row-num) ". "; }</style>');})(jQuery);
